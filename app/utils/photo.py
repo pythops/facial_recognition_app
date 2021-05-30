58242f33c68a8a35c8392d5344f4bcd039f1c66a
pythops <contact@pythops.com>
2021-05-30 20:50:56 +0200

    code refactoring

--- app/utils/photo.py
+++ app/utils/photo.py
@@ -1,13 +1,12 @@
-import dlib
 import cv2
+import dlib
 from flask import current_app
 
 
 def same_person(photo1, photo2):
     detector = dlib.get_frontal_face_detector()
     sp = dlib.shape_predictor(f"{current_app.config['MODEL_DIR']}/sp.dat")
-    facerec = dlib.face_recognition_model_v1(
-        f"{current_app.config['MODEL_DIR']}/fr.dat")
+    facerec = dlib.face_recognition_model_v1(f"{current_app.config['MODEL_DIR']}/fr.dat")
 
     p1 = dlib.load_rgb_image(photo1)
     p2 = dlib.load_rgb_image(photo2)
@@ -26,10 +25,12 @@ def same_person(photo1, photo2):
         shape = sp(p2, d)
         desc2 = facerec.compute_face_descriptor(p2, shape)
 
-    cv2.rectangle(p1, (face1[0].left(), face1[0].top()),
-                  (face1[0].right(), face1[0].bottom()), (0, 255, 0), 3)
-    cv2.rectangle(p2, (face2[0].left(), face2[0].top()),
-                  (face2[0].right(), face2[0].bottom()), (0, 255, 0), 3)
+    cv2.rectangle(
+        p1, (face1[0].left(), face1[0].top()), (face1[0].right(), face1[0].bottom()), (0, 255, 0), 3
+    )
+    cv2.rectangle(
+        p2, (face2[0].left(), face2[0].top()), (face2[0].right(), face2[0].bottom()), (0, 255, 0), 3
+    )
 
     p1 = cv2.cvtColor(p1, cv2.COLOR_BGR2RGB)
     p2 = cv2.cvtColor(p2, cv2.COLOR_BGR2RGB)
